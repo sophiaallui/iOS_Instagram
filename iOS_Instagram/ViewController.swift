93045f3f8047f6d4cf336c420670ceec2264a1d1
sophiaallui <sophiaallui@gmail.com>
2021-05-07 09:53:08 -0700

    Initial Commit

---
+++ iOS_Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Instagram
+//
+//  Created by Sophia Lui on 5/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
