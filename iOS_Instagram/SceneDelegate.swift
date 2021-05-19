7dfa07e67977caf4b39c7c338ae9023ac1272555
sophiaallui <sophiaallui@gmail.com>
2021-05-18 22:36:08 -0700

    worked on login logic, user can now remain logged in if they previously logged in

--- iOS_Instagram/SceneDelegate.swift
+++ iOS_Instagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,6 +18,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // Already logged in, navigate to feed navigation 
+        if PFUser.current() != nil {
+            // Main.storyboard
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(identifier: "FeedNavigationController")
+            window?.rootViewController = feedNavigationController
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
